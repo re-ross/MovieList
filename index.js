72e1c3d6ccd52a41d9c6a0d07dfc20494a4f4f02
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-01-19 15:36:45 -0600

    finished lab

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("Hello, world!");
+let message = document.querySelector("#message");
+
+function addMovie(event) {
+  event.preventDefault();
+
+  let inputField = document.querySelector("input");
+
+  if (!inputField.value) {
+    return alert("Gotta enter something");
+  }
+
+  let movie = document.createElement("li");
+  let movieTitle = document.createElement("span");
+  movieTitle.textContent = inputField.value;
+  movieTitle.addEventListener("click", crossOffMovie);
+  movie.appendChild(movieTitle);
+
+  let deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "X";
+  deleteBtn.addEventListener("click", deleteMovie);
+  movie.appendChild(deleteBtn);
+
+  let ul = document.querySelector("ul");
+  ul.appendChild(movie);
+  inputField.value = "";
+}
+let form = document.querySelector("form");
+form.addEventListener("submit", addMovie);
+
+function deleteMovie(event) {
+  event.target.parentNode.remove();
+  message.textContent = "Movie Deleted!";
+}
+
+function crossOffMovie(event) {
+  event.target.classList.toggle("checked");
+  if (event.target.classList.contains("checked")) {
+    message.textContent = "Movie watched!";
+  } else {
+    message.textContent = "Movie added back!";
+  }
+}
